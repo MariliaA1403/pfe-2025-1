6564ce49bb3035dc152c19ef2b1724523ea40a73
MariliaA1403 <178656215+MariliaA1403@users.noreply.github.com>
2025-02-17 23:55:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ ex01/app/page.js
@@ -0,0 +1,15 @@
+function Texto1() {
+  return <p>Um outro parágrafo de texto!</p>;
+}
+
+export default function Home() {
+  const aula = 2;
+  return (
+    <div>
+      <h1>Olá Turma!!!!</h1>
+      <p>Essa é a nossa {aula}ª aula de React!</p>
+      <Texto1 />
+      <Texto1 />
+    </div>
+  );
+}
